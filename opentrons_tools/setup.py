fdc546036d3a65d5c9b98f38dd95a7c4e0f28926
ibloise <ibloise62@gmail.com>
2023-01-19 21:43:47 +0100

    create gitignore

--- opentrons_tools/setup.py
+++ opentrons_tools/setup.py
@@ -2,10 +2,10 @@ from setuptools import find_packages
 from setuptools import setup
 
 setup(
-    name = 'Opentrons-tools',
+    name = 'opentrons_tools',
     version = '1.0.0',
     packages=find_packages('src'),
-    packages_dir={'':'src'},
+    package_dir={'':'src'},
     author = 'Iván Bloise Sánchez',
     description= 'Functions and other tools for Opentrons protocols'
 )
