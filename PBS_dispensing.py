fa46045360174cee60a112dd12450caeeca5f5f2
ibloise <ibloise62@gmail.com>
2023-01-19 08:33:49 +0100

    delete constants folder

--- PBS_dispensing.py
+++ PBS_dispensing.py
@@ -1,6 +1,5 @@
 from opentrons import protocol_api
-from constants.PBS_constants import *
-from opentrons_tools.distribute_tools import distribute_vol_and_offsets, reorder_distribute_dict
+
 
 import math
 # metadata
@@ -29,6 +28,37 @@ P100_FLOW_DISPENSE = 1000
 P1000_FLOW_BLOWOUT = 1000
 START_WELL = 0
 
+#Variables de configuracion
+## Mandar a un modulo
+PIPETTE_LABWARE_NAME_KEY = "pipette_labware_name"
+PIPETTE_LABEL_KEY = "pipette_label"
+PIPETTE_POSITION_KEY = "position"
+TIP_RACK_LABWARE_NAME_KEY = "tip_rack_labware_name"
+TIP_RACK_SLOT_LIST_KEY = "tip_rack_slots"
+TIP_RACK_LABEL_KEY = "tip_rack_label"
+LABWARE_NAME = 'labware_name'
+LABWARE_SLOTS = 'labware_slots'
+LABWARE_LABEL = 'labware_label'
+LABWARE_TYPE = 'labware_type'
+LEFT_POSITION = 'left'
+RIGHT_POSITION = 'right'
+TUBERACK_TYPE = 'tube_rack'
+TUBE_COUNT = 'tube_count'
+SLOT_LABEL_SUFFIX = ' slot '
+FALCON_DICT_PREFFIX = 'falcon_'
+FALCON_SLOT_KEY = 'slot'
+FALCON_DICT_KEY = 'falcon'
+ORDER_FALCON_KEY = 'order'
+OFFSET_KEY = 'offset'
+OFFSET_VALUE_KEY = 'offset_value'
+DEST_WELLS_KEY = 'dest_wells'
+SRC_LBW_SLOT_KEY = 'source_labware_slot'
+SRC_LBW_WELL_KEY = 'source_labware_well'
+DEST_LBW_SLOT_KEY = 'dest_labware_slot'
+DEST_LBW_WELL_KEY = "dest_labware_well"
+OFFSET_SLOT = 'offset_slot' 
+DISTRIBUTE_WELL_KEY = 'distribute_wells'
+
 # Dict labwares-> habría que pasarlo a clases
 
 EPPENDORF_LABWARE = {
@@ -54,6 +84,66 @@ P1000_PIPETTE = {
     TIP_RACK_SLOT_LIST_KEY : ['3'],
     TIP_RACK_LABEL_KEY : '1000µl tiprack'
 }
+
+#Funciones
+
+def distribute_vol_and_offsets(number_of_tubes, tubes_slot_list, source_slot_list, offset_iterates, 
+max_tube_by_rack, max_source_tube_by_rack, max_tubes_fill_by_iter, tube_key_preffix = 'tube_',
+tube_rack_slot_key = 'rack_tube_slot', tube_well_key = 'tube_well', falcon_tube_rack_slot_key = 'falcon_rack_slot',
+falcon_well_key = 'falcon_well', offset_key = 'offset'):
+    #Set variables
+    dist_dict={}
+    epp_rack_idx = 0
+    tube_well_idx = 0
+    falcon_rack_idx = 0
+    falcon_well_idx = 0
+    offset_idx = 0
+    tubes_fill_in_iter = 0
+    #iter by tubes
+    for tube in range(number_of_tubes):
+        tube_key = tube_key_preffix + str(tube)
+        dist_dict[tube_key] = {}
+        #set tubes rack and well
+        dist_dict[tube_key][tube_rack_slot_key] = tubes_slot_list[epp_rack_idx]
+        dist_dict[tube_key][tube_well_key] = tube_well_idx
+        tube_well_idx +=1
+        # control dest parameters
+        if tube_well_idx >= max_tube_by_rack:
+            epp_rack_idx +=1
+            tube_well_idx = 0
+        # set source well, rack and offset 
+        dist_dict[tube_key][falcon_tube_rack_slot_key] = source_slot_list[falcon_rack_idx]
+        dist_dict[tube_key][falcon_well_key] = falcon_well_idx
+        dist_dict[tube_key][offset_key] = offset_iterates[offset_idx]
+        tubes_fill_in_iter +=1
+        #control source parameters
+        if tubes_fill_in_iter >= max_tubes_fill_by_iter:
+            offset_idx +=1
+            tubes_fill_in_iter = 0
+            if offset_idx >=len(offset_iterates):
+                falcon_well_idx +=1
+                offset_idx = 0
+                if falcon_well_idx >= max_source_tube_by_rack:
+                    falcon_rack_idx +=1
+                    falcon_well_idx = 0
+    return(dist_dict)
+
+def reorder_distribute_dict(distribute_dict, labware_dict, falcon_rack_slot = 'falcon_rack_slot',
+    falcon_well = 'falcon_well', offset = 'offset', rack_slot_key='rack_tube_slot', tube_well_key = 'tube_well', 
+    distribute_well_key = 'distribute_well', pk_sep = "-" , bottom_distance = 5):
+        
+    new_key_builder = [falcon_rack_slot, falcon_well, offset]
+    reorder_dict = {} 
+    for dict in distribute_dict.values():
+        pklist = [str(dict[key]) for key in dict.keys() if key in new_key_builder]
+        pk = pk_sep.join(pklist)
+        order = labware_dict[dict[rack_slot_key]].wells()[dict[tube_well_key]].bottom(bottom_distance)
+        if pk not in reorder_dict.keys():
+            reorder_dict[pk] = {key: value for key, value in dict.items() if key in new_key_builder}
+            reorder_dict[pk][distribute_well_key] = [order]
+        else:
+            reorder_dict[pk][distribute_well_key].append(order)
+    return(reorder_dict)
 
 #Calculo de variables
 EPPENDORF_RACKS_NEEDED = math.ceil(NUMBER_OF_TUBES/EPPENDORF_LABWARE[TUBE_COUNT])
--- opentrons_tools/distribute_tools.py
+++ opentrons_tools/distribute_tools.py
@@ -41,14 +41,14 @@ falcon_well_key = 'falcon_well', offset_key = 'offset'):
 
 def reorder_distribute_dict(distribute_dict, labware_dict, falcon_rack_slot = 'falcon_rack_slot',
     falcon_well = 'falcon_well', offset = 'offset', rack_slot_key='rack_tube_slot', tube_well_key = 'tube_well', 
-    distribute_well_key = 'distribute_well', pk_sep = "-" ):
+    distribute_well_key = 'distribute_well', pk_sep = "-" , bottom_distance = 5):
         
     new_key_builder = [falcon_rack_slot, falcon_well, offset]
     reorder_dict = {} 
     for dict in distribute_dict.values():
         pklist = [str(dict[key]) for key in dict.keys() if key in new_key_builder]
         pk = pk_sep.join(pklist)
-        order = labware_dict[dict[rack_slot_key]].wells()[dict[tube_well_key]]
+        order = labware_dict[dict[rack_slot_key]].wells()[dict[tube_well_key]].bottom(bottom_distance)
         if pk not in reorder_dict.keys():
             reorder_dict[pk] = {key: value for key, value in dict.items() if key in new_key_builder}
             reorder_dict[pk][distribute_well_key] = [order]
