e3adc1c5c8ada36d71dddc70c2b1871903880f39
ibloise <ibloise62@gmail.com>
2023-01-30 10:34:33 +0100

    create constants file

--- FIS_opentrons_protocols/protocols/extraction_sybr_dispensing.py
+++ FIS_opentrons_protocols/protocols/extraction_sybr_dispensing.py
@@ -1,5 +1,6 @@
 from opentrons import protocol_api
-from FIS_opentrons_tools import *
+from FIS_opentrons_tools.distribute_tools import *
+from FIS_opentrons_tools.constants import *
 
 # metadata
 metadata = {
@@ -15,37 +16,19 @@ VERSION = '0.1.0'
 # samples
 
 NUMBER_OF_SAMPLES = 94
+MULTI_PIPETTE = True
 
 #Variables del programa
 MAX_SAMPLES_NUMBER = 94
 PRELOAD_VOLUME_UL = 60
 DILUTION_COCIENT = 10
+PCR_VOL_UL = 5
 P20_ASP_RATE_FIRST_STEP = 600
 P20_DISP_RATE_FIRST_STEP = 1000
 P20_BLOW_RATE_FIRST_STEP = 1000
 P20_ASP_RATE_SECOND_STEP = 600
 P20_DISP_RATE_SECOND_STEP = 1000
 P20_BLOW_RATE_SECOND_STEP = 1000
-
-
-#Variables de configuracion
-PIPETTE_LABWARE_NAME_KEY = "pipette_labware_name"
-PIPETTE_LABEL_KEY = "pipette_label"
-PIPETTE_POSITION_KEY = "position"
-TIP_RACK_LABWARE_NAME_KEY = "tip_rack_labware_name"
-TIP_RACK_SLOT_LIST_KEY = "tip_rack_slots"
-TIP_RACK_LABEL_KEY = "tip_rack_label"
-LABWARE_NAME = 'labware_name'
-LABWARE_SLOTS = 'labware_slots'
-LABWARE_LABEL = 'labware_label'
-LABWARE_TYPE = 'labware_type'
-LEFT_POSITION = 'left'
-RIGHT_POSITION = 'right'
-TUBERACK_TYPE = 'tube_rack'
-WELLPLATE_TYPE = 'well_plate'
-TUBE_COUNT = 'tube_count'
-WELLS_COUNT  = 'wells_count'
-SLOT_LABEL_SUFFIX = '_slot_'
 
 
 #Labwares
@@ -62,8 +45,18 @@ p20_pipette = {
     PIPETTE_POSITION_KEY : LEFT_POSITION,
     PIPETTE_LABEL_KEY : 'Opentrons P20 single gen2',
     TIP_RACK_LABWARE_NAME_KEY : 'opentrons_96_filtertiprack_20ul',
-    TIP_RACK_SLOT_LIST_KEY : ['3'],
+    TIP_RACK_SLOT_LIST_KEY : ['6'],
     TIP_RACK_LABEL_KEY : '20µl tiprack'
+}
+
+multip20_pipette = {    
+    PIPETTE_LABWARE_NAME_KEY :'p20_multi_gen2',
+    PIPETTE_POSITION_KEY : RIGHT_POSITION,
+    PIPETTE_LABEL_KEY : 'Opentrons P20 multi gen2',
+    TIP_RACK_LABWARE_NAME_KEY : 'opentrons_96_filtertiprack_20ul',
+    TIP_RACK_SLOT_LIST_KEY : ['3'],
+    TIP_RACK_LABEL_KEY : 'multichannel 20µl tiprack'
+
 }
 
 extraction_labw_plate_settings = {    
@@ -89,10 +82,6 @@ def is_integer_num(n):
         return n.is_integer()
     return False
 
-
-
-
-
 #Protocol
 def run(ctx: protocol_api.ProtocolContext):
 
@@ -111,14 +100,37 @@ def run(ctx: protocol_api.ProtocolContext):
     for slot in extraction_labw_plate_settings[LABWARE_SLOTS]
     }
 
+    pcr_plate = {str(slot) : ctx.load_labware(
+    pcr_labw_plate_settings[LABWARE_NAME], slot,
+    pcr_labw_plate_settings[LABWARE_LABEL] + SLOT_LABEL_SUFFIX + str(slot)) 
+    for slot in pcr_labw_plate_settings[LABWARE_SLOTS]
+    }
+
     tipracks20 = [
         ctx.load_labware(
             p20_pipette[TIP_RACK_LABWARE_NAME_KEY],slot, p20_pipette[TIP_RACK_LABEL_KEY] + f'_slot {str(index)}') 
-            for index, slot in enumerate(p20_pipette[TIP_RACK_SLOT_LIST_KEY])]
+            for index, slot in enumerate(p20_pipette[TIP_RACK_SLOT_LIST_KEY])
+            ]
+    multi_tiprack20 = [
+        ctx.load_labware(
+            multip20_pipette[TIP_RACK_LABWARE_NAME_KEY], slot, multip20_pipette[TIP_RACK_LABEL_KEY] + f'_slot {str(index)}')
+            for index, slot in enumerate(multip20_pipette[TIP_RACK_SLOT_LIST_KEY])
+    ]
 
-    # load pipette
-    p20 = ctx.load_instrument(
-        p20_pipette[PIPETTE_LABWARE_NAME_KEY], p20_pipette[PIPETTE_POSITION_KEY], tip_racks=tipracks20)
+    if MULTI_PIPETTE:
+        p20 = ctx.load_instrument(
+        p20_pipette[PIPETTE_LABWARE_NAME_KEY], p20_pipette[PIPETTE_POSITION_KEY], tip_racks=tipracks20
+        )
+
+        sec_pipette = ctx.load_instrument(
+            multip20_pipette[PIPETTE_LABWARE_NAME_KEY], multip20_pipette[PIPETTE_POSITION_KEY], tip_racks=multi_tiprack20
+        )
+    else:
+        p20 = ctx.load_instrument(
+        p20_pipette[PIPETTE_LABWARE_NAME_KEY], p20_pipette[PIPETTE_POSITION_KEY], tip_racks=tipracks20.append(multi_tiprack20)
+        )
+        sec_pipette = p20
+
     
     #****First step*****
     # get orders
@@ -165,4 +177,10 @@ def run(ctx: protocol_api.ProtocolContext):
 
     temp_mod.deactivate
 
-
+    sec_pipette.transfer(
+        volume = 5,
+        source=extraction_plate['1'].wells(),
+        dest = pcr_plate['2'].wells(),
+        disposal_volume=0,
+        new_tip = 'always'
+    )
---
+++ FIS_opentrons_protocols/tools/FIS_opentrons_tools/constants.py
@@ -0,0 +1,34 @@
+
+#Constants for labware definition
+
+PIPETTE_LABWARE_NAME_KEY = "pipette_labware_name"
+PIPETTE_LABEL_KEY = "pipette_label"
+PIPETTE_POSITION_KEY = "position"
+TIP_RACK_LABWARE_NAME_KEY = "tip_rack_labware_name"
+TIP_RACK_SLOT_LIST_KEY = "tip_rack_slots"
+TIP_RACK_LABEL_KEY = "tip_rack_label"
+LABWARE_NAME = 'labware_name'
+LABWARE_SLOTS = 'labware_slots'
+LABWARE_LABEL = 'labware_label'
+LABWARE_TYPE = 'labware_type'
+LEFT_POSITION = 'left'
+RIGHT_POSITION = 'right'
+TUBERACK_TYPE = 'tube_rack'
+WELLPLATE_TYPE = 'well_plate'
+WELLS_COUNT  = 'wells_count'
+TUBE_COUNT = 'tube_count'
+SLOT_LABEL_SUFFIX = '_slot_'
+FALCON_DICT_PREFFIX = 'falcon_'
+FALCON_SLOT_KEY = 'slot'
+FALCON_DICT_KEY = 'falcon'
+ORDER_FALCON_KEY = 'order'
+OFFSET_KEY = 'offset'
+OFFSET_VALUE_KEY = 'offset_value'
+DEST_WELLS_KEY = 'dest_wells'
+SRC_LBW_SLOT_KEY = 'source_labware_slot'
+SRC_LBW_WELL_KEY = 'source_labware_well'
+DEST_LBW_SLOT_KEY = 'dest_labware_slot'
+DEST_LBW_WELL_KEY = "dest_labware_well"
+OFFSET_SLOT = 'offset_slot' 
+DISTRIBUTE_WELL_KEY = 'distribute_wells'
+
